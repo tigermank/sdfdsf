8e8ac6d694267f38f8eca7005f8d6a2ab365f5fd
tigermank <41104892+tigermank@users.noreply.github.com>
2018-07-11 08:52:06 +0200

    Create sdfdsadas.js

---
+++ sdfdsadas.js
@@ -0,0 +1,117 @@
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<b><br /></b>
+<div style="text-align: center;">
+<b>ÇÚáÇä 1 åäÇ</b></div>
+<br />
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<div style="text-align: center;">
+<b><span style="font-size: large;">   åäÇ íßÊÈ äÕ Øæíá  </span></b><br />
+<b><span style="font-size: large;"><br /></span></b>
+<b><span style="font-size: large;">Ads-By-Google</span></b><br />
+ÇÚáÇä 1 åäÇ
+
+
+<br />
+<div style="text-align: left;">
+<span style="font-size: large;"><b>åäÇ íßÊÈ äÕ&nbsp;&nbsp;</b></span><br />
+<div style="text-align: center;">
+<br /></div>
+</div>
+</div>
+</div>
+</div>
+<div style="text-align: center;">
+<span style="font-size: large;"><b><br /></b></span></div>
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<div style="border-radius: 10px; border: 1px solid #e4e4e4; padding: 10px;">
+<div class="row margin-bottom-20">
+<div class="col-md-4 text-center">
+<h3>
+
+Ads-By-Google
+<br />
+
+<br />
+
+ÇÚáÇä 1 åäÇ
+
+
+
+</h3>
+</div>
+<div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h2>
+<i class="fa fa-shield"></i> ÌÇÑí ÝÍÕ ÇáÑÇÈØ ãä Çí ãÔÇßá... <i class="fa fa-shield"></i></h2>
+</div>
+<div class="panel-body text-center">
+<div class="text-primary margin-bottom-20">
+ÅäÊÙÑ ÞáíáÇ ááÍÕæá Úáí ÇáÑÇÈØ ÇáãØáæÈ</div>
+<div class="progress" id="daplong">
+<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
+</div>
+</div>
+<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <b> Êã ÈäÌÇÍ ! </b> Êã ÇáæÕæá Åáì ÇáÑÇÈØ ÇáãØáæÈ <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
+<br />
+<div class="panel-body">
+<script src="https://rawcdn.githack.com/kamalrajab2018/ffffffffffffffffffffffffffffffff/master/fffffffffffffffff/ffffffffffffff.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://surl2019.blogspot.com/p/blog-page.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong> download links </strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<h3>
+
+Ads-By-Google
+<br />
+
+<br />
+
+ÇÚáÇä 1 åäÇ
+
+
+
+
+</h3>
+</div>
+<div class="clear">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="text-left">
+</div>
+</div>
+</div>
+<br />
+<div style="text-align: center;">
+<br /></div>
+<div style="text-align: center;">
+<b><span style="font-size: large;">    åäÇ íßÊÈ äÕ ãÊæÓØ </span></b></div>
+<div style="text-align: left;">
+<br />
+<div style="text-align: center;">
+Ads-By-Google</div>
+<div style="text-align: center;">
+<br /></div>
+<div style="text-align: center;">
+ÇÚáÇä 1 åäÇ
+
+
+</div>
+<div style="text-align: center;">
+<br /></div>
+</div>
+<div style="text-align: left;">
+<div style="text-align: center;">
+<b><span style="font-size: large;">åäÇ íßÊÈ äÕ ÇíÖÇð</span></b></div>
+</div>
+</div>
